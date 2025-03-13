0000f9d5227b7afc02ef2a429085d6560331e5ed
MwiseHeritier <mwisehertier>
2025-03-13 11:40:52 +0200

    feat: created our first project with javascript

---
+++ intro.js
@@ -0,0 +1,12 @@
+let width = 4;
+let height = 5;
+
+console.log(`Width: ${width} cm`);
+console.log(`Height: ${height} cm`);
+
+area =width * height
+console.log(`Area: ${area} cm2`);
+
+
+let perimeter = 2* (width +height);
+console.log(`Perimeter: ${perimeter} cm`)

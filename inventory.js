77b5275f8314509a2a98555cf7800efe67c63978
MwiseHeritier <mwisehertier>
2025-03-26 17:48:15 +0200

    feat: completed inventory management system

--- inventory.js
+++ inventory.js
@@ -1,14 +1,32 @@
 let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry', 'kiwi', 'grape', 'watermelon', 'pineapple', 'blueberry'];
-let vegetables = ['carrot', 'tomato', 'pepper', 'lettuce'];
+let vegetables = ['carrot', 'tomato', 'pepper', 'lettuce', 'cucumber', 'brocol', 'onion', 'garlic', 'potato', 'ginger'
+];
 let fastfood = ['hamburger', 'fries', 'hotdog', 'pizza'];
 
 console.log("=== Inventory Management System ===");
-console.log("--- fruits ---");
+console.log("--- Fruits ---");
 
 for (let i= 0; i < fruits.length; i++){
     let j = i + 1;
     console.log(' ' + j + ' ' + fruits[i]);
 }
+console.log("----------------");
+console.log(`Total number of fruits: ${fruits.length}`);
+
+console.log("---Vegetables---");
+let n = 0;
+while(n < vegetables.length){
+    let m = n + 1;
+    console.log(' ' + m + ' ' + vegetables[n]);
+    n++;
+}
+console.log("----------------");
+console.log(`Total number of fruits: ${vegetables.length}`);
+console.log("---Fast Food---");
+fastfood.forEach((food,index)=> {
+    console.log(' ' +(index +1)+ ' ' +food);
+});
+console.log(`Total number of Fast food: ${fastfood.length}`);
 console.log("----------------");
 console.log("=================================");
 

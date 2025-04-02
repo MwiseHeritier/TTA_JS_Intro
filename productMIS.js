9fe356b0b4199ff0acb340a32128e406736fa527
MwiseHeritier <mwisehertier>
2025-04-02 16:20:31 +0200

    feat understood the key concepts of OOP

--- productMIS.js
+++ productMIS.js
@@ -16,7 +16,7 @@ class Product{
 }
 
 
-let product1 = new Product ('Laptop', 50000, 'Electronics');
+let product1 = new Product ('T-Shirt', 1000, 'Clothing');
 let product2 = new Product ('Shirt', 500, 'Clothing');
 
 product1.getInfo();
@@ -24,4 +24,29 @@ product1.applyDiscount(10);
 product2.getInfo();
 product2.applyDiscount(20);
 
+
+class Electronics extends Product{
+    constructor(name, price,brand, waranty){
+        super(name, price,'Electronics' );
+        this.brand = brand;
+        this.waranty = waranty;
+    }
+    getInfo(){
+        console.log(`\n\t Name: ${this.name} \n\t Price: ${this.price} \n\t Category: ${this.category}`);
+        console.log(`\t Brand: ${this.brand} \n\t waranty: ${this.waranty}`);
+    }
+        
+    }
+    
+
+let product3 = new  Electronics('Laptop', 50000, 'Dell', '1 year');
+let product4 = new  Electronics('Mobile', 20000, 'Samsung', '2 years');
+
+product3.getInfo();
+//product3.getElecrtonicsInfo();
+product3.applyDiscount(10);
+
+product4.getInfo();
+//product4.getElecrtonicsInfo();
+product4.applyDiscount(10);
 
